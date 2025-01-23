5916317a3d604d590f6aa6a3db11a09221e41756
LovekUR <23bcs13984@cuchd.in>
2025-01-23 16:15:06 +0000

    operations

---
+++ 01_basic/03_converstionoperation.js
@@ -0,0 +1,47 @@
+let score = "33abc"
+
+// console.log(typeof score)
+
+let valueInNumber = Number(score);
+// console.log(valueInNumber);
+
+//to boolean
+
+let isLoggedIn = 1
+
+let booleanIsLogged = Boolean(isLoggedIn)
+// console.log(booleanIsLogged)
+
+//convert to string
+
+let number = 22
+let StringNum = String(number)
+// console.log(StringNum)
+// console.log(typeof StringNum)
+
+// ************operations***************
+
+let value = 3
+let negValue = -value
+// console.log(negValue)
+
+let str = "Hello"
+let str2 = "everyone"
+let str3 = str + " "+ str2
+// console.log(str3);
+
+
+console.log("1"+ 2)
+console.log(1 + "2")
+console.log("1"+"2")
+console.log("1"+2+2)// first is string so everthing will now treated as string
+console.log(1+2+"3")//first is number so they will treated as number and thus they will add up and become 3 and in end 3 will be attached with them as it is string
+
+let num1, num2, num3
+num1 = num2 = num3 = 5
+
+let gamecounter = 100
+gamecounter++
+console.log(gamecounter)
+
+
