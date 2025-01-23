13693553632e9159e532af6c3a0c35b5bee9b7ec
LovekUR <23bcs13984@cuchd.in>
2025-01-23 15:08:26 +0000

    datatypes

---
+++ 01_basic/01_datatypes.js
@@ -0,0 +1,8 @@
+"use strict"; //treat JS code as newer version
+
+let name = "Lovepreet"
+let age = 87
+let isLoggedIn = false
+
+console.log(typeof undefined)
+console.log(typeof null) // null is stand alone value and it is an object
